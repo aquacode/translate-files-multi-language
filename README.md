6c7d4d756cc5f5c17781ce8300a1ff2938b42424
Michael Brizic <michael.brizic@stagrp.com>
2021-04-15 11:00:28 -0500

    Initial code

---
+++ README.md
@@ -0,0 +1,36 @@
+# Setup
+
+pip install virtualenv
+virtualenv <your-env>
+source <your-env>/bin/activate
+<your-env>/bin/pip install -r requirements.txt
+
+
+# Required Environment Variables
+
+GOOGLE_APPLICATION_CREDENTIALS -- this is set to fully qualified path to Google API JSON credentials file
+GOOGLE_PARENT_PROJECT: set to your Google project that has Google Translate API enabled
+
+
+# How To Use
+
+python main.py translate {filename} {languages} where:
+
+{filename} is a fully qualified path to json or properties file to be translated
+{languages} is a space separate list of language codes 
+
+
+# How It Works
+
+* Parses either the JSON or Properties file given and translates each value into each of the language codes passed
+* The resulting translations are written to a file of the same type i.e. json or properties, with the name equivalent to name of the original file followed by a hyphen and the language code for the translation
+
+## Example:
+
+Input:
+python main.py translate messages.properties es ar
+
+Output:
+messages.properties (original file)
+messages-es.properties (Spanish version)
+messages-ar.properties (Arabic version)
---
+++ main.py
@@ -0,0 +1,121 @@
+# stdlib
+from datetime import datetime
+import json
+import os
+import os.path
+import sys
+
+# third party
+from google.cloud import translate
+from jproperties import Properties
+import click
+
+# globals
+CURR_LANGUAGE = None
+client = translate.TranslationServiceClient()
+GOOGLE_PARENT_PROJECT = None
+
+
+def printMap(m):
+    for k,v in m.items():
+        if isinstance(v, dict):
+            print(k)
+            printMap(v)
+        elif isinstance(v, str):
+            print("{}: {}".format(k, v))
+
+def convertStr(data, ignore_dicts=False):
+    if isinstance(data, str):
+        print("translate: {} to language: {}".format(data, CURR_LANGUAGE))
+        response = client.translate_text(parent=GOOGLE_PARENT_PROJECT, mime_type="text/plain", target_language_code=CURR_LANGUAGE, contents=[data])
+        trans = response.translations
+        print("result: {} \n".format(trans[0].translated_text))
+        return trans[0].translated_text
+    if isinstance(data, list):
+        return [convertStr(item, ignore_dicts=True) for item in data]
+    if isinstance(data, dict) and not ignore_dicts:
+        return {
+            key: convertStr(value, ignore_dicts=True) for key, value in data.items()
+        }
+    return data
+
+def propsTranslator(inputfile, languages):
+    global CURR_LANGUAGE
+    for curr in languages:
+        CURR_LANGUAGE = curr
+        props = Properties()
+        with open(inputfile, 'rb') as f:
+            props.load(f, encoding='utf-8')
+            newProps = Properties()
+            for k,v in props.items():
+                print("translate: {} to language: {}".format(v.data, CURR_LANGUAGE))
+                response = client.translate_text(parent=GOOGLE_PARENT_PROJECT, mime_type="text/plain", target_language_code=CURR_LANGUAGE, contents=[v.data])
+                trans = response.translations
+                print("result: {} \n".format(trans[0].translated_text))
+                newProps[k] = trans[0].translated_text
+
+            base = os.path.basename(inputfile)
+            filename = os.path.splitext(base)[0]
+            with open(filename+'-'+CURR_LANGUAGE+'.properties', 'wb') as newF:
+                newProps.store(newF, encoding='utf-8')
+
+def jsonTranslator(inputfile, languages):
+    global CURR_LANGUAGE
+    for curr in languages:
+        CURR_LANGUAGE = curr
+        print("Translating to language: {}".format(CURR_LANGUAGE))
+        with open(inputfile, 'r') as f:
+            data = json.load(f, object_hook=convertStr)
+
+        base = os.path.basename(inputfile)
+        filename = os.path.splitext(base)[0]
+        with open(filename+'-'+CURR_LANGUAGE+'.json', 'w') as w:
+            print("Writing out translated file")
+            json.dump(data, w, ensure_ascii=False, indent=2)
+
+# Class that allows aliases for commands
+class CustomMultiCommand(click.Group):
+    def command(self, *args, **kwargs):
+        def decorator(f):
+            if isinstance(args[0], list):
+                _args = [args[0][0]] + list(args[1:])
+                for alias in args[0][1:]:
+                    cmd = super(CustomMultiCommand, self).command(alias, *args[1:], **kwargs)(f)
+                    cmd.short_help = "Alias for '{}'".format(_args[0])
+            else:
+                _args = args
+            cmd = super(CustomMultiCommand, self).command(*_args, **kwargs)(f)
+            return cmd
+
+        return decorator
+
+# Main Entrypoint Function
+@click.group(cls=CustomMultiCommand)
+def main():
+    pass
+
+@main.command(["translate"])
+@click.argument("filename", type=click.Path(exists=True))
+@click.argument("languages", nargs=-1)
+def translate(filename, languages):
+
+    global GOOGLE_PARENT_PROJECT
+    creds = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+    if not creds:
+        sys.exit("GOOGLE_APPLICATION_CREDENTIALS is required!")
+    proj = os.getenv('GOOGLE_PARENT_PROJECT')
+    if not proj:
+        sys.exit("GOOGLE_PARENT_PROJECT is required!")
+    else:
+        GOOGLE_PARENT_PROJECT = proj
+
+    if languages:
+        if filename.endswith('json'):
+            jsonTranslator(filename, languages)
+        elif filename.endswith('properties'):
+            propsTranslator(filename, languages)
+    else:
+        print("Empty list of languages")
+
+if __name__ == '__main__':
+    main()
